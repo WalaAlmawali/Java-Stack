72bea52eb01f8251cf11a01ffe1f5c14a71187aa
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-23 11:32:38 +0400

    Simulate visiting at least 5 different web pages using push() + Display the current page and all pages in history after each visit

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -5,6 +5,20 @@ public class BrowserHistorySimulator {
 
         Stack<String> BrowserHistory = new Stack<>();
 
+        BrowserHistory.push("google.com");
+        System.out.println(BrowserHistory);
+
+        BrowserHistory.push("youtube.com");
+        System.out.println(BrowserHistory);
+
+        BrowserHistory.push("github.com");
+        System.out.println(BrowserHistory);
+
+        BrowserHistory.push("stackoverflow.com");
+        System.out.println(BrowserHistory);
+
+        BrowserHistory.push("wikipedia.org");
+        System.out.println(BrowserHistory);
 
     }
 
