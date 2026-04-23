722ab642d64baf422af5459047589c57cde6a6c6
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-23 10:55:30 +0400

    Add  5 employee action records using push()

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -4,5 +4,12 @@ public class EmployeeStackDemo {
    public static void main(String[] args){
       Stack<String> stack = new Stack<>();
 
+      stack.push("Employee John added to HR department");
+      stack.push("Employee Sara promoted to Team Leader");
+      stack.push("Employee Ali transferred to Finance department");
+      stack.push("Employee Maria updated personal details");
+      stack.push("Employee Ahmed completed training program");
+
+
    }
 }
