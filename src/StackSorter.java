3b54e859fbf424bce6bac8d3ff335865c28da565
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-27 15:20:19 +0400

    Update while condetion , It will keep run while tempStack is not empty and top element in tempStack is greater than current + Create stack object + call sortStack function in the main method

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -1,26 +1,35 @@
 import java.util.Stack;
 public class StackSorter {
     public static void main(String[] args) {
+        Stack<Integer> s1 = new Stack<>();
+        s1.push(3);
+        s1.push(1);
+        s1.push(4);
+        s1.push(2);
 
+        System.out.println(sortStack(s1));
 
 
     }
 
 
-    public static Stack<Integer> sortStack(Stack<Integer> stack){
+    public static Stack<Integer> sortStack(Stack<Integer> stack) {
         Stack<Integer> tempStack = new Stack<>();
 
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             int current = stack.pop();
 
-            while(!tempStack.isEmpty() && tempStack.peek()< current){
+            while (!tempStack.isEmpty() && tempStack.peek() > current) {
 
                 stack.push(tempStack.pop());
             }
+            tempStack.push(current);
 
         }
 
+        return tempStack;
     }
-
-
 }
+
+
+
