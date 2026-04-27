be444cc53b1d915da37deb15708ea8ffc60c324f
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-27 11:33:25 +0400

    Call findNextGreaterElement,findNextGreaterElementCircular and findNextSmallerElement in main method

--- src/NextGreaterElementFinder.java
+++ src/NextGreaterElementFinder.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 public class NextGreaterElementFinder {
     public static void main(String[] args) {
 
+        int [] arr =   {4, 5, 2, 25};
+        System.out.println(Arrays.toString(findNextGreaterElement(arr)));
+        System.out.println(Arrays.toString(findNextGreaterElementCircular(arr)));;
+        System.out.println(Arrays.toString(findNextSmallerElement(arr)));;
 
     }
 
