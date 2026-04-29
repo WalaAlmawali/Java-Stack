90f66a6214d0e717429dc41c43215cd59601ee01
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-29 09:26:40 +0400

    Declare width variable

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -21,6 +21,8 @@ public class LargestRectangleInHistogram {
 
             }else{
                 int top = indicesOfHistogram.pop();
+                int height = heights[top];
+                int width;
             }
 
         }
