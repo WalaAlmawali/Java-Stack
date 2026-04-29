ed13428624c8d20c19ce26b0f2fcc196617cde68
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-29 10:23:03 +0400

    Add two foreach to loop throgh array elements

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -1,85 +1,92 @@
 import java.util.Stack;
 public class LargestRectangleInHistogram {
     public static void main(String[] args) {
-        int [] arr = {2,1,5,6,2,3};
+        int[] arr = {2, 1, 5, 6, 2, 3};
         System.out.println(findLargestRectangle(arr));
 
     }
-    public static int findLargestRectangle(int[] heights){
 
-        if (heights.length == 0){
+    public static int findLargestRectangle(int[] heights) {
+
+        if (heights.length == 0) {
             return 0;
         }
-         Stack<Integer> indicesOfHistogram = new Stack<>();
+        Stack<Integer> indicesOfHistogram = new Stack<>();
 
         int maxArea = 0;
         int i = 0;
-        while(i< heights.length){
+        while (i < heights.length) {
 
-            if(indicesOfHistogram.isEmpty()){
+            if (indicesOfHistogram.isEmpty()) {
                 indicesOfHistogram.push(i);
                 i++;
             }
 
-            if(heights[i]>= heights[indicesOfHistogram.peek()]){
+            if (heights[i] >= heights[indicesOfHistogram.peek()]) {
                 indicesOfHistogram.push(i);
                 i++;
 
-            }else{
+            } else {
                 int top = indicesOfHistogram.pop();
                 int height = heights[top];
                 int width;
 
-                if(indicesOfHistogram.isEmpty()){
+                if (indicesOfHistogram.isEmpty()) {
                     width = i;
-                }else{
+                } else {
                     width = i - indicesOfHistogram.peek() - 1;
                 }
 
-                int area = calculateArea(height,width);
+                int area = calculateArea(height, width);
 
-                maxArea = Math.max(maxArea,area);
+                maxArea = Math.max(maxArea, area);
 
             }
         }
 
-        while (!indicesOfHistogram.isEmpty()){
+        while (!indicesOfHistogram.isEmpty()) {
 
             int top = indicesOfHistogram.pop();
             int height = heights[top];
             int width;
-            if(indicesOfHistogram.isEmpty()){
+            if (indicesOfHistogram.isEmpty()) {
                 width = i;
-            }else{
+            } else {
                 width = i - indicesOfHistogram.peek() - 1;
             }
-            int area = calculateArea(height,width);
+            int area = calculateArea(height, width);
 
-            maxArea = Math.max(maxArea,area);
+            maxArea = Math.max(maxArea, area);
         }
 
         return maxArea;
     }
 
-    public static int calculateArea(int heights, int width){
-        return heights * width ;
+    public static int calculateArea(int heights, int width) {
+        return heights * width;
 
     }
-    public static void displayHistogram(int[] heights){
+
+    public static void displayHistogram(int[] heights) {
         System.out.println("Histogram");
         int max = 0;
-        for (int h: heights)  max = Math.max(max,h);
+        for (int h : heights) max = Math.max(max, h);
 
-        for(int i = max; i> 0; i--){
+        for (int i = max; i > 0; i--) {
 
-            for(int h : heights){
-                if(h >= i) System.out.print("*");
+            for (int h : heights) {
+                if (h >= i) System.out.print("*");
                 else System.out.print("");
-        }
+            }
             System.out.println();
 
+        }
+
+        for (int h : heights) System.out.print("---");
+        System.out.println();
+
+        for (int h : heights) System.out.print("" + h + "");
+        System.out.println("\n");
+
     }
-
-
-
-    }
+}
