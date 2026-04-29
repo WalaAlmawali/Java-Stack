3e6aebab5b31814a856d48c1a1d96fd21869d0d2
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-29 09:06:49 +0400

    Create a Stack to store indices of histogram bars

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -9,6 +9,8 @@ public class LargestRectangleInHistogram {
         if (heights.length == 0){
             return 0;
         }
+         Stack<Integer> indicesOfHistogram = new Stack<>();
+
     }
 
 
