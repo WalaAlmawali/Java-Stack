d769ace8e95d853ea89457db838b1026fcd9cc2d
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-30 14:22:30 +0400

    Create class named Position to represent a queen's position

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -4,7 +4,16 @@ public class NQueensSolver {
     public static void main(String[] args) {
 
 
-
     }
 
+    // Class to represent a queen's position
+    class Position {
+        int row, col;
+
+        public Position(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
     }
+}
