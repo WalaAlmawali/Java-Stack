77c36dcdceb4e6f581c19417e6dfd5cd0e2debc5
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-30 14:27:06 +0400

    Implement isSafe(Position pos, Stack<Position> queens ) function for Check if

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -1,22 +1,28 @@
 import java.util.Stack;
+// Class to represent a queen's position
+class Position {
+    int row, col;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+    public String toString(){
+        return "("+row + ","+ col + ")";
+    }
+
+}
 
 public class NQueensSolver {
-    public static void main(String[] args) {
+   static boolean isSafe(Position pos, Stack<Position> queens ){
 
 
-    }
 
-    // Class to represent a queen's position
-    class Position {
-        int row, col;
+   }
 
-        public Position(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-        public String toString(){
-            return "("+row + ","+ col + ")";
-        }
 
-    }
 }
+        public static void main(String[] args) {
+
+
+        }
